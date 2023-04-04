d0f5eec8dea3b023cca09ac7a79586c2eda64dc3
= <=>
2023-04-05 06:06:48 +0700

    implement template dengan php

---
+++ tugas_MSIB_php_implement_template/menu.php
@@ -0,0 +1,33 @@
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <a class="nav-link" href="index.php?url=dashboard">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <a class="nav-link" href="index.php?url=data">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Data
+                    </a>
+                    <a class="nav-link" href="index.php?url=product">
+                        <div class="sb-nav-link-icon"><i class="fas fa-box"></i></div>
+                        Product
+                    </a>
+                    <a class="nav-link" href="index.php?url=about">
+                        <div class="sb-nav-link-icon"><i class="fas fa-address-card"></i></div>
+                        About
+                    </a>
+                    <a class="nav-link" href="index.php?url=contact">
+                        <div class="sb-nav-link-icon"><i class="fas fa-address-book"></i></div>
+                        Contact
+                    </a>
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Start Bootstrap
+            </div>
+        </nav>
+    </div>
